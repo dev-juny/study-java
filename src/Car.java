f5ffa269b93ef9a16edc41cecb9d85711609ab6b
dev-juny <121218647+dev-juny@users.noreply.github.com>
2023-04-20 21:17:32 +0900

    toString() 오버라이딩

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    public void run(){
+        System.out.println("전륜구동으로 달리다.");
+    }
+
+    //오버라이딩
+    @Override
+    public String toString() {
+        return "자동차";
+    }
+}
---
+++ src/CarExam3.java
@@ -0,0 +1,15 @@
+public class CarExam3 {
+    public static void main(String[] args) {
+
+        Car c1 = new Car();
+        System.out.println("c1 = " + c1); //println(Object x) == System.out.println(c1.torString()))
+
+    }
+}
+
+//부모 타입의 변수로 자식인스턴스를 참조할 수 있다.
+//조상타입의 변수로 후손인스턴스를 참조할 수 있다.
+
+//Car c1 = new Bus();
+//Car c2 = new 이층버스(); //이층버스는 Car의 자손이다.
+
